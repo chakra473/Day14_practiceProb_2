090e34a446482138526a2c8626c16baff6564eb8
chakravarthy <m.chakra473@gmail.com>
2022-03-26 22:26:20 +0530

    initiated

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace day14_practiceProb2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to linkedList problem!");
+        }
+    }
+}
